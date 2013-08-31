6722917d227d79665429e3c48ede284ecb501a61
Jürgen Jakobitsch <turnguard@linux-1rgw.site>
2013-08-31 19:39:04 +0200

    fix deploy debian script's cd

--- src/bin/deploy-debian.sh
+++ src/bin/deploy-debian.sh
@@ -6,7 +6,7 @@ FILES=${project.artifactId}*.deb
 for f in $FILES
 do
     DEBIAN_PACKAGE="`pwd`/$f"
-    cd DEBIAN_REPOSITORY_ROOT
+    cd $DEBIAN_REPOSITORY_ROOT
     reprepro remove lucid ${project.artifactId}
     reprepro includedeb lucid $DEBIAN_PACKAGE
 done
