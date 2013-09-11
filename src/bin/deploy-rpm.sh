f0012e52e5eb3596810a6c4b1ae2a8fcba674664
Christian Mader <mail@christianmader.net>
2013-09-11 15:53:13 +0200

    rpm deployment and repository generation

--- src/bin/deploy-rpm.sh
+++ src/bin/deploy-rpm.sh
@@ -4,14 +4,5 @@ echo "DEPLOYING RPM"
 
 RPM_REPOSITORY_ROOT="/var/www/rpm"
 
-#cd "target"
-#FILES=${project.artifactId}*.deb
-#for f in $FILES
-#do
-#    DEBIAN_PACKAGE="`pwd`/$f"
-#    cd $DEBIAN_REPOSITORY_ROOT
-#    reprepro remove lucid ${project.artifactId}
-#    reprepro includedeb lucid $DEBIAN_PACKAGE
-#done
-
-#echo "DEPLOYED"
+cp target/*.rpm $RPM_REPOSITORY_ROOT
+createrepo $RPM_REPOSITORY_ROOT
