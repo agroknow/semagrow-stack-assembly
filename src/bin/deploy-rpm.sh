1a74a282477019276eb56fe951a173ce83ae6d94
Christian Mader <mail@christianmader.net>
2013-09-12 14:34:16 +0200

    changed keyname to use semagrow keypair

--- src/bin/deploy-rpm.sh
+++ src/bin/deploy-rpm.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+echo "SIGNING PACKAGE"
+rpm-sign target/*.rpm
+
 echo "DEPLOYING RPM"
-
 RPM_REPOSITORY_ROOT="/var/www/rpm"
-
 cp target/*.rpm $RPM_REPOSITORY_ROOT
 createrepo $RPM_REPOSITORY_ROOT
+
